30de7b4f3fde71d334c8e3fc40982852939869fe
Joao Dias <joao.dias@midway.com.br>
2020-06-25 04:13:51 -0300

    improvment route

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { index, view } from "./controller/Stocks";
 
 const routes = Router();
 
-routes.get("/stocks/", index);
+routes.get("/stocks", index);
 routes.get("/stocks/:stockName", view);
 
 export default routes;
